bc83dfaa186fc424bf2d29a643f3da937779586e
wyh900207 <wyh900207@126.com>
2017-03-18 12:36:53 +0800

    抽象Tabbar,方便index中直接使用

---
+++ Components/Tabbar.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TabBarIOS,
+} from 'react-native';
+import Styles from '../Styles/MainStyles';
+import Dimensions from 'Dimensions';
+import Icons from '../Assert/Icons';
+import Featured from './Featured';
+import MostView from './MostView'
+
+export default class Tabbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      selectedTab: 'featured'
+    }
+  }
+  render() {
+    return(
+      <TabBarIOS barTintColor='darkslateblue' tintColor='white'>
+        <TabBarIOS.Item icon={{uri: Icons.qrCode, scale: 14}} 
+                        title='第一个'
+                        selected={this.state.selectedTab === 'featured'}
+                        onPress={() => {
+                          this.setState({
+                            selectedTab: 'featured'
+                          })
+                        }}
+                        >
+          <Featured />
+        </TabBarIOS.Item>
+        <TabBarIOS.Item icon={{uri: Icons.mostViewed, scale: 4.6}}
+                        title='第二个'
+                        selected={this.state.selectedTab === 'most-viewed'}
+                        onPress={() => {
+                          this.setState({
+                            selectedTab: 'most-viewed'
+                          });
+                        }}
+                        >
+          <MostView />
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  }
+}
